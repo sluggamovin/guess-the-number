b05f2bc88a667f7e4aa1e2728f99503d265b3d1f
sluggamovin <mauricewright10@yahoo.com>
2025-11-13 11:59:16 -0500

    Update v1.py
    Fixed errors with parenthesis

--- v1.py
+++ v1.py
@@ -3,9 +3,9 @@ import random as rd
 magic_number = rd.randint(1, 10)
 print(magic_number)
 
-guess = int (input("Guess a number between 1 and 10: ")
+guess = int(input("Guess a number between 1 and 10: "))
 
-if guess == magic_number :
-   print("You win!"
+if guess == magic_number:
+   print("You win!")
 else:
    print("You lose!")
