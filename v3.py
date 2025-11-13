154b34d027d909b4fca666f7cc4c241e8baab247
sluggamovin <mauricewright10@yahoo.com>
2025-11-13 12:10:56 -0500

    Update v3.py

--- v3.py
+++ v3.py
@@ -1,1 +1,20 @@
+import random as rd
 
+counter = 0
+magic_number = rd.randint(1, 10)
+print(magic_number)
+
+guess = int(input("Guess a number between 1 and 10: "))
+counter += 1
+
+while guess != magic_number and counter < 5:
+  guess = int(input("Guess a number between 1 and 10: "))
+  counter += 1
+  
+  if guess == magic_number:
+     print("You win!")
+  else:
+     print("Incorrect!")
+     
+if counter >= 5:
+  print("You lose!")
